8136479e9aa027dd167484f8c38457d71eabf1ac
tochman <thomas@nocebo.se>
2015-09-16 10:33:49 +0200

    bumped rails version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
@@ -10,7 +9,6 @@ class User < ActiveRecord::Base
   validates_presence_of :first_name
   validates_presence_of :last_name
 
-  # attr_accessor :name
     def name
       [self.first_name, self.last_name].join(" ")
     end
